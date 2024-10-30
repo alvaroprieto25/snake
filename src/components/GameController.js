3e0f64c437daef4f38e47bfa892bc9efda44bafe
Álvaro Prieto <apm226@alu.ua.es>
2024-10-30 20:04:35 +0100

    Add controls with awsd

--- src/components/GameController.js
+++ src/components/GameController.js
@@ -111,16 +111,16 @@ const GameController = () => {
       if(!moveCompleted) return;
 
       switch (e.keyCode) {
-        case 38:
+        case 38, 83:
           if (direction !== 'DOWN') setDirection('UP');
           break;
-        case 40:
+        case 40, 87:
           if (direction !== 'UP') setDirection('DOWN');
           break;
-        case 37:
+        case 37, 68:
           if (direction !== 'RIGHT') setDirection('LEFT');
           break;
-        case 39:
+        case 39, 65:
           if (direction !== 'LEFT') setDirection('RIGHT');
           break;
         default:
